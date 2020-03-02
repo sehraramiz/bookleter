f7c1e42f55fca318ca2fc8129d9dc9800b05e25c
ali <mohsennbarzegar@gmail.com>
2020-03-02 13:17:46 +0330

    !imp cleanup

--- pytools.py
+++ pytools.py
@@ -1,3 +1,4 @@
+import random
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
@@ -6,8 +7,8 @@ def pickout_pages(input_pdf_name, output_pdf_name, start_page_number, end_page_n
     output = PdfFileWriter()
     for pg_number in range(start_page_number, end_page_number + 1):
         output.addPage(inputpdf.getPage(pg_number))
-    with open(output_pdf_name, "wb") as outputStream:
-        output.write(outputStream)
+    with open(output_pdf_name, "wb") as output_stream:
+        output.write(output_stream)
 
 def append_blank_pages(input_pdf_name, output_pdf_name, blanK_pages_count):
     inputpdf = PdfFileReader(open(input_pdf_name, "rb"))
@@ -15,5 +16,6 @@ def append_blank_pages(input_pdf_name, output_pdf_name, blanK_pages_count):
     output.appendPagesFromReader(inputpdf)
     for i in range(blanK_pages_count):
         output.addBlankPage()
-    with open(output_pdf_name, "wb") as outputStream:
+    with open(output_pdf_name, "wb") as output_stream:
+        output.write(output_stream)
         output.write(outputStream)
