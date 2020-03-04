081e2526eb94776837375cfc94c54e5b8801f0cc
ali <mohsennbarzegar@gmail.com>
2020-03-04 22:25:29 +0330

    updated setup.py added setup.cfg

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup
 setup(
     name = 'bookleter',
     version = '0.1.0',
+    license='MIT',
+    description = 'turns pdfs into a6 sized foldable booklets',
+    author = 'Mohsen Barzegar',
+    author_email = 'mohsennbarzegar@gmail.com',
+    url = 'https://github.com/reinenichts/bookleter',
+    download_url = 'https://github.com/reinenichts/bookleter/archive/first.tar.gz',
+    keywords = ['book', 'booklet', 'pdf'],
     packages = ['bookleter'],
+    install_requires=[
+            'PyPDF2',
+        ],
     entry_points = {
         'console_scripts': [
             'bookleter = bookleter.__main__:main'
