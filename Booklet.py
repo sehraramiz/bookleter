0cdb47da9292192f36ad559b7802eb9466edc5ad
ali <mohsennbarzegar@gmail.com>
2020-03-04 15:19:07 +0330

    moved booklet creation process to a seprate class for future use

---
+++ Booklet.py
@@ -0,0 +1,121 @@
+import logging, subprocess, shutil
+from pathlib import Path
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from shuffle import foop
+
+
+logging.basicConfig(level=logging.NOTSET)
+
+
+class Book():
+    def __init__(self, input_file_path, start_page_number, end_page_number, direction, margins):
+        self.input_file_path = input_file_path
+        self.start_page_number = start_page_number
+        self.end_page_number = end_page_number
+        self.direction = direction
+        self.margins = margins
+
+        self.temp_path = self.input_file_path.parent / 'tmp'
+        Path(self.temp_path).mkdir(parents=True, exist_ok=True)
+
+        self.original_pdf_name = self.input_file_path.name
+        self.original_pdf_path = str(self.input_file_path)
+        self.margined_pdf_name = str(self.temp_path / self.original_pdf_name.replace(".pdf", "_margined.pdf"))
+        self.pickout_pages_pdf_name = self.margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(start_page_number, end_page_number))
+        self.pickout_test_pages_pdf_name = self.margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(1, 8))
+        self.reversed_pickout_test_pages_pdf_name = self.pickout_test_pages_pdf_name.replace(".pdf", "_reversed.pdf")
+        self.blanked_pdf_name = self.pickout_pages_pdf_name.replace(".pdf", "_blanked.pdf")
+        self.reversed_blanked_pdf_name = self.blanked_pdf_name.replace(".pdf", "_reversed.pdf")
+        self.final_pdf_name = self.original_pdf_path.replace(".pdf", "_print_this.pdf")
+        self.test_pdf_name = self.final_pdf_name.replace(".pdf", "_for_test.pdf")
+
+    def make_booklet(self):
+        logging.info("setting  margins...")
+        self._set_margin_crop()
+        logging.info("picking out desired pages...")
+        self._pickout_pages(self.start_page_number, self.end_page_number)
+
+        self.end_page_number = (self.end_page_number - self.start_page_number) + 1
+        self.start_page_number = 1
+
+        self.correct_pages_count, self.blank_pages_count = self._calc_pdf_pages()
+
+        if self.blank_pages_count:
+            logging.info("adding blank pages...")
+            self._append_blank_pages()
+        else:
+            logging.info("no need for extra blank pages...")
+            self.blanked_pdf_name = self.pickout_pages_pdf_name
+
+        if self.direction == "rtl":
+            logging.info("changing book direction to rtl...")
+            self._reverse_pages_order()
+
+        logging.info("shuffling pages order...\ncreating final pdf...")
+        print_order = foop(self.reversed_blanked_pdf_name, self.final_pdf_name, self.correct_pages_count)
+        self._shuffle_pdf(print_order)
+
+        logging.info("creating test pdf...")
+        self._pickout_pages(1, 8)
+
+        if self.direction == "rtl":
+            self._reverse_pages_order()
+
+        print_order = foop(self.reversed_pickout_test_pages_pdf_name, self.test_pdf_name, 8)
+        self._shuffle_pdf(print_order)
+        
+        logging.info("cleaning up...")
+        shutil.rmtree(self.temp_path)
+
+        logging.info("finished!")
+
+    def _pickout_pages(self, start_page_number, end_page_number):
+        inputpdf = PdfFileReader(open(self.margined_pdf_name, "rb"))
+        output = PdfFileWriter()
+        for pg_number in range(start_page_number, end_page_number + 1):
+            output.addPage(inputpdf.getPage(pg_number))
+        with open(self.pickout_pages_pdf_name, "wb") as output_stream:
+            output.write(output_stream)
+
+    def _append_blank_pages(self):
+        inputpdf = PdfFileReader(open(self.pickout_pages_pdf_name, "rb"))
+        output = PdfFileWriter()
+        output.appendPagesFromReader(inputpdf)
+        for i in range(self.blank_pages_count):
+            output.addBlankPage()
+        with open(self.blanked_pdf_name, "wb") as output_stream:
+            output.write(output_stream)
+
+    def _reverse_pages_order(self):
+        output = PdfFileWriter()
+        with open(self.blanked_pdf_name, 'rb') as readfile:
+            inputpdf = PdfFileReader(readfile)
+            for page in reversed(inputpdf.pages):
+                output.addPage(page)
+            with open(self.reversed_blanked_pdf_name, "wb") as output_stream:
+                output.write(output_stream)
+
+    def _shuffle_pdf(self, ordered_pages):
+        output = PdfFileWriter()
+        inputpdf = PdfFileReader(open(self.reversed_blanked_pdf_name, "rb"))
+        for pg_number in ordered_pages:
+            output.addPage(inputpdf.getPage(pg_number - 1))
+        with open(self.final_pdf_name, "wb") as output_stream:
+            output.write(output_stream)
+
+    def _calc_pdf_pages(self):
+        if self.end_page_number % 8 == 0:
+            correct_pages_count = self.end_page_number
+        else:
+            correct_pages_count = ((((self.end_page_number - self.start_page_number) + 1) // 8) + 1) * 8
+        white_pages_count = correct_pages_count - self.end_page_number
+        return correct_pages_count, white_pages_count
+
+    def _set_margin_crop(self):
+        ## set margin or crop
+        ## '10 7 10 7' --> 'left top right bottom'
+        ## example command: pdfcrop in.pdf out.pdf --margins '10 7 10 7'
+        margin_command = "pdfcrop {} {} --margins '{}'".format(self.original_pdf_name, self.margined_pdf_name, self.margins)
+        subprocess.call([
+            margin_command,
+            ], shell=True)
--- bookleter.py
+++ bookleter.py
@@ -1,10 +1,11 @@
 #!/bin/python3
 
-import subprocess, sys
+import subprocess, sys, shutil
 from pathlib import Path, PurePath
 from shuffle import foop
 from pytools import pickout_pages as pick, append_blank_pages, reverse_pages_order as reverse, shuffle_pdf as shuffle, calc_pdf_pages
 from tools import set_margin_crop, check_requirments
+from Booklet import Book
 
 
 example_usage_command = """
@@ -34,47 +35,52 @@ file_path = PurePath.joinpath(current_path, sys.argv[1])
 temp_path = file_path.parent / 'tmp'
 Path(temp_path).mkdir(parents=True, exist_ok=True)
 
-# not very important file names
-original_pdf_name = file_path.name
-original_pdf_path = str(file_path)
-margined_pdf_name = str(temp_path / original_pdf_name.replace(".pdf", "_margined.pdf"))
-pickout_pages_pdf_name = margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(start_page_number, end_page_number))
-pickout_test_pages_pdf_name = margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(1, 8))
-reversed_pickout_test_pages_pdf_name = pickout_test_pages_pdf_name.replace(".pdf", "_reversed.pdf")
-blanked_pdf_name = pickout_pages_pdf_name.replace(".pdf", "_blanked.pdf")
-reversed_blanked_pdf_name = blanked_pdf_name.replace(".pdf", "_reversed.pdf")
-final_pdf_name = original_pdf_path.replace(".pdf", "_print_this.pdf")
-test_pdf_name = final_pdf_name.replace(".pdf", "_for_test.pdf")
+NewBook = Book(file_path, start_page_number, end_page_number, book_direction, margins)
+NewBook.make_booklet()
 
-set_margin_crop(original_pdf_path, margined_pdf_name, margins)
+# # not very important file names
+# original_pdf_name = file_path.name
+# original_pdf_path = str(file_path)
+# margined_pdf_name = str(temp_path / original_pdf_name.replace(".pdf", "_margined.pdf"))
+# pickout_pages_pdf_name = margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(start_page_number, end_page_number))
+# pickout_test_pages_pdf_name = margined_pdf_name.replace(".pdf", "_{}_{}.pdf".format(1, 8))
+# reversed_pickout_test_pages_pdf_name = pickout_test_pages_pdf_name.replace(".pdf", "_reversed.pdf")
+# blanked_pdf_name = pickout_pages_pdf_name.replace(".pdf", "_blanked.pdf")
+# reversed_blanked_pdf_name = blanked_pdf_name.replace(".pdf", "_reversed.pdf")
+# final_pdf_name = original_pdf_path.replace(".pdf", "_print_this.pdf")
+# test_pdf_name = final_pdf_name.replace(".pdf", "_for_test.pdf")
 
-# pickout only desired pages from original pdf
-pick(margined_pdf_name, pickout_pages_pdf_name, start_page_number, end_page_number)
+# set_margin_crop(original_pdf_path, margined_pdf_name, margins)
 
-end_page_number = (end_page_number - start_page_number) + 1
-start_page_number = 1
+# # pickout only desired pages from original pdf
+# pick(margined_pdf_name, pickout_pages_pdf_name, start_page_number, end_page_number)
 
-correct_pages_count, blank_pages_count = calc_pdf_pages(start_page_number, end_page_number)
+# end_page_number = (end_page_number - start_page_number) + 1
+# start_page_number = 1
 
-if blank_pages_count:
-    append_blank_pages(pickout_pages_pdf_name, blanked_pdf_name, blank_pages_count)
-else:
-    blanked_pdf_name = pickout_pages_pdf_name
+# correct_pages_count, blank_pages_count = calc_pdf_pages(start_page_number, end_page_number)
 
-if book_direction == "rtl":
-    reverse(blanked_pdf_name, reversed_blanked_pdf_name)
+# if blank_pages_count:
+#     append_blank_pages(pickout_pages_pdf_name, blanked_pdf_name, blank_pages_count)
+# else:
+#     blanked_pdf_name = pickout_pages_pdf_name
 
-print_order = foop(reversed_blanked_pdf_name, final_pdf_name, correct_pages_count)
-shuffle(reversed_blanked_pdf_name, final_pdf_name, print_order)
+# if book_direction == "rtl":
+#     reverse(blanked_pdf_name, reversed_blanked_pdf_name)
+
+# print_order = foop(reversed_blanked_pdf_name, final_pdf_name, correct_pages_count)
+# shuffle(reversed_blanked_pdf_name, final_pdf_name, print_order)
 
 # create a 8 page pdf for testing the printer device and print method before printing big chunks of paper
 # extract pages 1 to 8 for 8 page test
-pick(margined_pdf_name, pickout_test_pages_pdf_name, 1, 8)
+# pick(margined_pdf_name, pickout_test_pages_pdf_name, 1, 8)
 
-if book_direction == "rtl":
-    reverse(pickout_test_pages_pdf_name, reversed_pickout_test_pages_pdf_name)
+# if book_direction == "rtl":
+#     reverse(pickout_test_pages_pdf_name, reversed_pickout_test_pages_pdf_name)
 
-print_order = foop(reversed_pickout_test_pages_pdf_name, test_pdf_name, 8)
-shuffle(reversed_pickout_test_pages_pdf_name, test_pdf_name, print_order)
+# print_order = foop(reversed_pickout_test_pages_pdf_name, test_pdf_name, 8)
+# shuffle(reversed_pickout_test_pages_pdf_name, test_pdf_name, print_order)
+
+# shutil.rmtree(temp_path)
 
 shutil.rmtree(temp_path)
