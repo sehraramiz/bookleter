7dfcdfc794069fa31c58f589368033dbed96a582
ali <mohsennbarzegar@gmail.com>
2020-03-08 16:07:51 +0330

    created basic gui that can browse pdf file and make the booklet

--- bookleter/gui.py
+++ bookleter/gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gui-qt/bookleter-gui.ui'
+# Form implementation generated from reading ui file 'bookleter-gui.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.1
 #
@@ -8,9 +8,10 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from Booklet import Book
 
 
-class Ui_main_window(object):
+class MainWindow(QtWidgets.QWidget):
     def setupUi(self, main_window):
         main_window.setObjectName("main_window")
         main_window.resize(240, 320)
@@ -23,37 +24,37 @@ class Ui_main_window(object):
         self.end_page_number = QtWidgets.QTextEdit(self.centralwidget)
         self.end_page_number.setGeometry(QtCore.QRect(160, 70, 41, 31))
         self.end_page_number.setObjectName("end_page_number")
-        self.margins = QtWidgets.QTextEdit(self.centralwidget)
-        self.margins.setGeometry(QtCore.QRect(90, 150, 51, 31))
-        self.margins.setObjectName("margins")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 40, 52, 13))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(20, 80, 61, 16))
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(140, 80, 16, 16))
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(20, 120, 52, 13))
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(20, 160, 52, 13))
-        self.label_5.setObjectName("label_5")
+        self.margins_percentage = QtWidgets.QTextEdit(self.centralwidget)
+        self.margins_percentage.setGeometry(QtCore.QRect(90, 150, 51, 31))
+        self.margins_percentage.setObjectName("margins_percentage")
+        self.input_file_label = QtWidgets.QLabel(self.centralwidget)
+        self.input_file_label.setGeometry(QtCore.QRect(20, 40, 52, 13))
+        self.input_file_label.setObjectName("input_file_label")
+        self.start_page_number_label = QtWidgets.QLabel(self.centralwidget)
+        self.start_page_number_label.setGeometry(QtCore.QRect(20, 80, 61, 16))
+        self.start_page_number_label.setObjectName("start_page_number_label")
+        self.end_page_number_label = QtWidgets.QLabel(self.centralwidget)
+        self.end_page_number_label.setGeometry(QtCore.QRect(140, 80, 16, 16))
+        self.end_page_number_label.setObjectName("end_page_number_label")
+        self.book_direction_label = QtWidgets.QLabel(self.centralwidget)
+        self.book_direction_label.setGeometry(QtCore.QRect(20, 120, 52, 13))
+        self.book_direction_label.setObjectName("book_direction_label")
+        self.margins_label = QtWidgets.QLabel(self.centralwidget)
+        self.margins_label.setGeometry(QtCore.QRect(20, 160, 52, 13))
+        self.margins_label.setObjectName("margins_label")
         self.make_booklet_button = QtWidgets.QPushButton(self.centralwidget)
         self.make_booklet_button.setGeometry(QtCore.QRect(60, 240, 111, 21))
         self.make_booklet_button.setObjectName("make_booklet_button")
-        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(90, 110, 51, 22))
-        self.comboBox.setObjectName("comboBox")
+        self.book_direction_combobox = QtWidgets.QComboBox(self.centralwidget)
+        self.book_direction_combobox.setGeometry(QtCore.QRect(90, 110, 51, 22))
+        self.book_direction_combobox.setObjectName("book_direction_combobox")
         self.progress_bar = QtWidgets.QProgressBar(self.centralwidget)
         self.progress_bar.setGeometry(QtCore.QRect(20, 200, 181, 23))
         self.progress_bar.setProperty("value", 24)
         self.progress_bar.setObjectName("progress_bar")
-        self.browse = QtWidgets.QPushButton(self.centralwidget)
-        self.browse.setGeometry(QtCore.QRect(90, 30, 111, 31))
-        self.browse.setObjectName("browse")
+        self.browse_button = QtWidgets.QPushButton(self.centralwidget)
+        self.browse_button.setGeometry(QtCore.QRect(90, 30, 111, 31))
+        self.browse_button.setObjectName("browse_button")
         main_window.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(main_window)
         self.statusbar.setObjectName("statusbar")
@@ -61,28 +62,57 @@ class Ui_main_window(object):
 
         self.retranslateUi(main_window)
         self.make_booklet_button.clicked.connect(self.make_booklet)
+        self.browse_button.clicked.connect(self.browse_file)
         QtCore.QMetaObject.connectSlotsByName(main_window)
 
+        self.book_direction_combobox.addItems(["rtl", "ltr"])
+        self.pdf_file_path = ""
+
+
     def make_booklet(self):
-        print("clicked!")
+        print(self.book_direction_combobox.currentText())
+        msg = QtWidgets.QMessageBox(self)
+        if len(self.pdf_file_path):
+            new_book = Book(
+                self.pdf_file_path,
+                int(self.start_page_number.toPlainText()),
+                int(self.end_page_number.toPlainText()),
+                self.book_direction_combobox.currentText(),
+                self.margins_percentage.toPlainText())
+            new_book.make_booklet()
+            msg.setText('Your book is ready!')
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+            msg.show()
+        else:
+            msg.setText('Please select a pdf file with browse button')
+            msg.setIcon(QtWidgets.QMessageBox.Warning)
+            msg.show()
+
+    def browse_file(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QtWidgets.QFileDialog.getOpenFileName()", "","Pdf Files (*.pdf)", options=options)
+        if fileName:
+            self.pdf_file_path = fileName
+            print(fileName)
 
     def retranslateUi(self, main_window):
         _translate = QtCore.QCoreApplication.translate
-        main_window.setWindowTitle(_translate("main_window", "MainWindow"))
-        self.label.setText(_translate("main_window", "your pdf"))
-        self.label_2.setText(_translate("main_window", "from page"))
-        self.label_3.setText(_translate("main_window", "to"))
-        self.label_4.setText(_translate("main_window", "direction"))
-        self.label_5.setText(_translate("main_window", "margins"))
+        main_window.setWindowTitle(_translate("main_window", "Bookleter"))
+        self.input_file_label.setText(_translate("main_window", "your pdf"))
+        self.start_page_number_label.setText(_translate("main_window", "from page"))
+        self.end_page_number_label.setText(_translate("main_window", "to"))
+        self.book_direction_label.setText(_translate("main_window", "direction"))
+        self.margins_label.setText(_translate("main_window", "margins"))
         self.make_booklet_button.setText(_translate("main_window", "Make My Booklet"))
-        self.browse.setText(_translate("main_window", "Browse"))
+        self.browse_button.setText(_translate("main_window", "Browse"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     main_window = QtWidgets.QMainWindow()
-    ui = Ui_main_window()
+    ui = MainWindow()
     ui.setupUi(main_window)
     main_window.show()
     sys.exit(app.exec_())
