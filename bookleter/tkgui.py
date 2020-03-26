ed524ef5b09fc82cb657101bd0f72a92b40fc137
ali <mohsennbarzegar@gmail.com>
2020-03-27 01:41:35 +0430

    added basic ui elements with pygubu

--- bookleter/tkgui.py
+++ bookleter/tkgui.py
@@ -1,26 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+import pygubu
 
-class Application(tk.Frame):
+class Application:
     def __init__(self, master=None):
-        super().__init__(master)
+        # super().__init__(master)
         self.master = master
-        self.pack()
-        self.create_widgets()
+        
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('tkgui.ui')
+        self.mainwindow = builder.get_object('mainwindow', master)
+        builder.connect_callbacks(self)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-    def say_hi(self):
-        print("hi there, everyone!")
 
 def gui_main():
     root = tk.Tk()
     app = Application(master=root)
-    app.mainloop()
+    root.mainloop()
 
 if __name__ == "__main__":
     gui_main()
