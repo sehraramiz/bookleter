dc5b07251b659384d94bdf8e228c28452b0948c1
ali <mohsennbarzegar@gmail.com>
2020-03-28 00:22:37 +0430

    updated version :>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'bookleter',
-    version = '0.4.1',
+    version = '0.4.2',
     license='MIT',
     description = 'Turns pdfs into a6 sized foldable booklets',
     author = 'Mohsen Barzegar',
