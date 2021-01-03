3d3d12bdb3487d33b5ecef3ac033fecad770868e
Mohsen Barzegar <mohsennbarzegar@gmail.com>
2021-01-03 21:07:40 +0330

    updated setup.py dependencies, urls and project version

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ setup(
     description = 'Turns pdfs into a6 sized foldable booklets',
     author = 'Mohsen Barzegar',
     author_email = 'mohsennbarzegar@gmail.com',
-    url = 'https://github.com/reinenichts/bookleter',
-    download_url = 'https://github.com/reinenichts/bookleter/archive/gui.tar.gz',
+    url = 'https://github.com/sehraramiz/bookleter',
+    download_url = 'https://github.com/sehraramiz/bookleter/archive/gui.tar.gz',
     keywords = ['book', 'booklet', 'pdf'],
     packages = ['bookleter'],
     install_requires=[
             'PyPDF2',
             'Pillow',
             'pygubu',
-            # 'PyQt5'
+            'pdfrw',
         ],
     entry_points = {
         'console_scripts': [
